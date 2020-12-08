8ae7287e3a3c7579e2b5b138cfb37e9ef2445883
MaoYue <849540142@qq.com>
2020-12-08 14:58:57 +0800

    在 config 目录下添加 status 配置文件，用于存放业务状态码

---
+++ tp/config/status.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 存放业务状态码
+ */
+
+return [
+    // 成功
+    "success" => 1,
+    // 失败
+    "error" => 2,
+    // 方法未找到
+    "way_not_found" => 3,
+    // 控制器未找到
+    "controller_not_found" => 4
+];
