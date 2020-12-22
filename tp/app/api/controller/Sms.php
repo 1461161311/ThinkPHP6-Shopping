70e486af5c3a8972269136e3bdfc52d9c14de927
MaoYue <849540142@qq.com>
2020-12-22 18:20:50 +0800

    短信验证码功能对接接口，对手机号进行验证

---
+++ tp/app/api/controller/Sms.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace app\api\controller;
+
+use app\BaseController;
+use app\common\business\Sms as SmsBusiness;
+
+class Sms extends BaseController
+{
+    public function code(): object
+    {
+        // 获取手机号码
+        $phoneNumber = input('param.phone_number', '', 'trim');
+
+        // 将获取到的手机号码放入数组，交给validate验证
+        $data = [
+            'phone_number' => $phoneNumber,
+        ];
+
+        // 验证数据
+        try {
+            validate(\app\api\validate\User::class)->scene("send_code")->check($data);
+            // 抛出异常
+        } catch (\think\Exception\ValidateException $exception) {
+            return show(config("status.error"), $exception->getError());
+        }
+
+        // 验证手机号是否符合规范
+        $g = "/^1[34578]\d{9}$/";
+        $g2 = "/^19[89]\d{8}$/";
+        $g3 = "/^166\d{8}$/";
+        if (preg_match($g, $phoneNumber) || preg_match($g2, $phoneNumber) || preg_match($g3, $phoneNumber)) {
+            // 成功验证手机号符合规范
+            // 调用business层的数据
+            if (SmsBusiness::sendCode($phoneNumber)) {
+                return show(config("status.success"), "发送验证码成功");
+            }
+            return show(config("status.error"), "发送验证码失败");
+        }
+
+        // 手机号验证失败
+        return show(config("status.error"), "手机号输入错误，请重新输入！");
+
+    }
+}
+
---
+++ tp/app/common/business/Sms.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace app\common\business;
+
+use app\common\lib\sms\SubmailSms;
+
+class Sms
+{
+    public static function sendCode(string $phoneNumber): bool
+    {
+        // 随口生成短信验证码 rand() 随机在两个值之间生成一个值
+        $code = rand(0000,9999);
+
+        $sms = SubmailSms::sendCode($phoneNumber,$code);
+
+        if ($sms){
+            // 需要把短信验证码记录到redis中，并且给出生效时间，如：该验证码一分钟失效
+
+
+        }
+        return $sms;
+    }
+
+
+}
+
