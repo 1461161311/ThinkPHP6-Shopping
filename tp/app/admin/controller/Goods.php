db2c896d0f61ef1af5e7788b72c12db4411ded25
MaoYue <849540142@qq.com>
2021-01-20 10:00:42 +0800

    后台商品管理页面增删改查

--- tp/app/admin/controller/Goods.php
+++ tp/app/admin/controller/Goods.php
@@ -4,25 +4,89 @@ namespace app\admin\controller;
 
 use app\admin\validate\Goods as GoodsValidate;
 use app\common\business\Goods as GoodsBus;
+use app\common\business\CategoryBus as CategoryBus;
+use think\facade\View;
 
 class Goods extends AdminBase
 {
     /**
-     * 商品列表页
-     * @return \think\response\View
+     * 商品列表
+     * @return string
      */
     public function index()
     {
-        return view();
+        $data = [];
+        $title = input("param.title", "", "trim");
+        $time = input("param.time", "", "trim");
+        $ti = $time;
+
+        if (!empty($title)) {
+            $data['title'] = $title;
+        }
+        if (!empty($time)) {
+            $data['create_time'] = explode(" - ", $time);
+        }
+
+        $result = (new GoodsBus())->getLists($data, 3);
+
+        if (empty($data['title'])) {
+            $data['title'] = "";
+        }
+        if (empty($data['create_time'])) {
+            $data['create_time'] = "";
+        }
+
+        $data['create_time'] = $ti;
+
+        return View::fetch("", [
+            "goods" => $result,
+            "search" => $data,
+        ]);
     }
 
     /**
+     * 修改商品状态
+     * @return \think\response\Json
+     * @throws \think\Exception
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function status()
+    {
+        $id = input("param.id", 0, "intval");
+        // 商品状态码
+        $status = input("param.status", 3, "intval");
+        // 商品是否首页推荐
+        $index = input("param.is_index_recommend", 3, "intval");
+
+        $data = [
+            "status" => $status,
+            "is_index_recommend" => $index,
+        ];
+
+        // 验证是要修改状态码还是修改是否首页推荐
+        if ($data['status'] == 0 || $data['status'] == 1 || $data['status'] == 99) {
+            $result = (new GoodsBus())->status($id, $data['status']);
+        } else {
+            $result = (new GoodsBus())->isIndex($id, $data['is_index_recommend']);
+        }
+
+        // 验证结果
+        if ($result) {
+            return show(config("status.success"), "状态更新成功");
+        }
+        return show(config("status.error"), "状态更新失败");
+    }
+
+
+    /**
      * 商品添加页
-     * @return \think\response\View
+     * @return string
      */
     public function add()
     {
-        return view();
+        return View::fetch();
     }
 
 
@@ -42,10 +106,10 @@ class Goods extends AdminBase
         $data = input("param.");
 
         // 验证页面的 token , 防止 csrf 攻击
-        $check = $this->request->checkToken('__token__');
-        if (!$check){
-            return show(config("status.error"),"非法请求");
-        }
+//        $check = $this->request->checkToken('__token__');
+//        if (!$check) {
+//            return show(config("status.error"), "非法请求");
+//        }
 
         // 验证参数
         $validate = new GoodsValidate();
@@ -54,7 +118,7 @@ class Goods extends AdminBase
             if (!$validate->scene('goods')->check($data)) {
                 return show(config("status.error"), $validate->getError());
             }
-        // 当商品选择多规格时
+            // 当商品选择多规格时
         } else {
             if (!$validate->scene('goods_sku')->check($data)) {
                 return show(config("status.error"), $validate->getError());
@@ -88,6 +152,103 @@ class Goods extends AdminBase
             return show(config("status.error"), "商品新增失败");
         }
         return show(config("status.success"), "商品新增成功");
+    }
+
+
+    /**
+     * 编辑页面
+     * @return string|\think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function update()
+    {
+        // 获取 id 查询要修改的数据
+        $id = input("param.id", 0, "intval");
+
+        // 查询数据
+        $result = (new GoodsBus())->getById($id);
+
+        // 分割分类信息,方便在页面回显
+        $path = explode(',', $result['category_path_id']);
+        $res = (new CategoryBus());
+        $arr = [];
+        // 以商品所属分类 id , 查询分类信息
+        foreach ($path as $value) {
+            $category = $res->getById($value);
+            $arr[] = $category['name'];
+        }
+
+        // 页面回显分类
+        $result['category_path_name'] = $arr;
+        // 页面回显轮播图
+        $carousel_image['image'] = explode(',', $result['carousel_image']);
+
+        return View::fetch("", [
+            "goods" => $result,
+            // 回显轮播图
+            "carousel_image" => $carousel_image,
+        ]);
+
+    }
+
+
+    /**
+     * 编辑功能
+     * @return \think\response\Json
+     * @throws \think\Exception
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function saveUpdate()
+    {
+        // 验证请求
+        if (!$this->request->isPost()) {
+            show(config("status.error"), "请求类型错误，非POST请求");
+        }
+
+        // 接收参数
+        $data = input("param.");
+
+        // 验证参数
+        $validate = new GoodsValidate();
+        if (!$validate->scene('saveUpdate')->check($data)) {
+            return show(config("status.error"), $validate->getError());
+        }
+
+        // 当重新选择分类时
+        if (!empty($data['category_id'])) {
+            // 处理数据
+            // category_path_id 是商品的分类路径,保存了多个分类id.最后一位id,就是商品所属分类
+            $data['category_path_id'] = $data['category_id'];
+            $result = explode(",", $data['category_path_id']);  // explode():将字符串分割成数组
+            $data['category_id'] = end($result);    // end():取数组的最后一位
+        }
+
+        // 处理数据,页面中的字段与数据库字段不同
+        $data['price'] = $data['market_price'];
+        $data['cost_price'] = $data['sell_price'];
+        // 删除不需要的字段
+        unset($data['sell_price'], $data['market_price']);
+
+        // 当页面并没有修改图片时
+        if ($data['carousel_image'] == "") {
+            unset($data['carousel_image']);
+        }
+        if ($data['recommend_image'] == "") {
+            unset($data['recommend_image']);
+        }
+
+        // 调用 business 层修改方法
+        $result = (new GoodsBus())->saveUpdate($data['id'], $data);
+
+        // 验证数据
+        if ($result) {
+            return show(config("status.success"), "商品编辑成功");
+        }
+        return show(config("status.error"), "商品编辑失败");
     }
 
 }
--- tp/runtime/admin/temp/2a11af1b9c9b928d451d374d005a47c2.php
+++ tp/runtime/admin/temp/2a11af1b9c9b928d451d374d005a47c2.php
@@ -1,4 +1,4 @@
-<?php /*a:1:{s:80:"E:\Code\Git-tp6\ThinkPHP6-Shopping_Project\tp\app\admin\view\category\index.html";i:1609940098;}*/ ?>
+<?php /*a:1:{s:80:"E:\Code\Git-tp6\ThinkPHP6-Shopping_Project\tp\app\admin\view\category\index.html";i:1610782477;}*/ ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -31,9 +31,7 @@
 <body>
 <div class="layuimini-container">
     <div class="layuimini-main">
-        <a href="/admin/category/add">
-            <button type="button" class="layui-btn add">添 加</button>
-        </a>
+        <button type="button" class="layui-btn add">添 加</button>
         <a href="<?php echo url('index',['pid' => 0]); ?>">
             <button>分类首页</button>
         </a>
@@ -42,77 +40,78 @@
             <button><?php echo htmlentities($vo['name']); ?></button>
         </a>
         <?php endforeach; endif; else: echo "" ;endif; ?>
-        </div>
-        <div class="layui-form" style="margin-top: 20px;">
-            <table class="layui-table">
-                <colgroup>
-                    <col width="60">
-                    <col width="60">
-                    <col width="60">
-                    <col width="130">
-                    <col width="130">
-                    <col width="70">
-                    <col width="70">
-                    <col width="185">
-                </colgroup>
-                <thead>
-                <tr>
-                    <th style="text-align:center;">编号</th>
-                    <th style="text-align:center;">标题</th>
-                    <th style="text-align:center;">排序</th>
-                    <th style="text-align:center;">创建时间</th>
-                    <th style="text-align:center;">更新时间</th>
-                    <th style="text-align:center;">操作人</th>
-                    <th style="text-align:center;">状 态</th>
-                    <th style="text-align:center;">操作管理</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php if(is_array($categorys['data']) || $categorys['data'] instanceof \think\Collection || $categorys['data'] instanceof \think\Paginator): $i = 0; $__LIST__ = $categorys['data'];if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?>
-                <tr>
-                    <td align="center">
-                        <?php echo htmlentities($vo['id']); ?>
-                    </td>
-                    <td align="center">
-                        <?php echo htmlentities($vo['name']); ?>
-                    </td>
-                    <td align="center">
-                        <div class="layui-input-inline">
-                            <input type="text" value="<?php echo htmlentities($vo['listorder']); ?>" data-id="<?php echo htmlentities($vo['id']); ?>"
-                                   class="changeSort layui-input">
-                        </div>
-                    </td>
-                    <td align="center">
-                        <?php echo htmlentities($vo['create_time']); ?>
-                    </td>
-                    <td align="center">
-                        <?php echo htmlentities($vo['update_time']); ?>
-                    </td>
-                    <td align="center">
-                        <?php echo htmlentities($vo['operate_user']); ?>
-                    </td>
-
-                    <td align="center" data-id="<?php echo htmlentities($vo['id']); ?>}"><input type="checkbox" <?php if($vo['status']== 1): ?>checked <?php endif; ?>
-                        name="status"
-                        lay-skin="switch"
-                        lay-filter="switchStatus"
-                        lay-text="ON|OFF">
-                    </td>
-                    <td align="center">
-                        <a class="layui-btn layui-btn-xs layui-btn-normal"
-                           href="<?php echo url('update',['id' => $vo['id']]); ?>">编辑</a>
-                        <a class="layui-btn layui-btn-xs layui-btn-danger data-count-delete delete" data-ptype="1"
-                           lay-event="delete" data-id="<?php echo htmlentities($vo['id']); ?>}">删除</a>
-                        <a class="layui-btn layui-btn-xs layui-btn-normal" href="<?php echo url('index',['pid' => $vo['id']]); ?>">子栏目
-                            (<?php echo htmlentities($vo['childCount']); ?>)</a>
-                    </td>
-                </tr>
-                <?php endforeach; endif; else: echo "" ;endif; ?>
-                </tbody>
-            </table>
-        </div>
-        <div id="pages"></div>
     </div>
+    <div class="layui-form" style="margin-top: 20px;">
+        <table class="layui-table">
+            <colgroup>
+                <col width="60">
+                <col width="60">
+                <col width="60">
+                <col width="130">
+                <col width="130">
+                <col width="70">
+                <col width="70">
+                <col width="185">
+            </colgroup>
+            <thead>
+            <tr>
+                <th style="text-align:center;">编号</th>
+                <th style="text-align:center;">标题</th>
+                <th style="text-align:center;">排序</th>
+                <th style="text-align:center;">创建时间</th>
+                <th style="text-align:center;">更新时间</th>
+                <th style="text-align:center;">操作人</th>
+                <th style="text-align:center;">状 态</th>
+                <th style="text-align:center;">操作管理</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php if(is_array($categorys['data']) || $categorys['data'] instanceof \think\Collection || $categorys['data'] instanceof \think\Paginator): $i = 0; $__LIST__ = $categorys['data'];if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?>
+            <tr>
+                <td align="center">
+                    <?php echo htmlentities($vo['id']); ?>
+                </td>
+                <td align="center">
+                    <?php echo htmlentities($vo['name']); ?>
+                </td>
+                <td align="center">
+                    <div class="layui-input-inline">
+                        <input type="text" value="<?php echo htmlentities($vo['listorder']); ?>" data-id="<?php echo htmlentities($vo['id']); ?>"
+                               class="changeSort layui-input">
+                    </div>
+                </td>
+                <td align="center">
+                    <?php echo htmlentities($vo['create_time']); ?>
+                </td>
+                <td align="center">
+                    <?php echo htmlentities($vo['update_time']); ?>
+                </td>
+                <td align="center">
+                    <?php echo htmlentities($vo['operate_user']); ?>
+                </td>
+
+                <td align="center" data-id="<?php echo htmlentities($vo['id']); ?>}"><input type="checkbox" <?php if($vo['status']== 1): ?>checked <?php endif; ?>
+                    name="status"
+                    lay-skin="switch"
+                    lay-filter="switchStatus"
+                    lay-text="ON|OFF">
+                </td>
+                <td align="center">
+                    <button type="button" class="layui-btn layui-btn-xs layui-btn-normal update" data-id="<?php echo htmlentities($vo['id']); ?>">
+                        编辑
+                    </button>
+                    <a class="layui-btn layui-btn-xs layui-btn-danger data-count-delete delete" data-ptype="1"
+                       lay-event="delete" data-id="<?php echo htmlentities($vo['id']); ?>">删除</a>
+                    <a class="layui-btn layui-btn-xs layui-btn-normal" href="<?php echo url('index',['pid' => $vo['id']]); ?>">子栏目
+                        (<?php echo htmlentities($vo['childCount']); ?>)</a>
+                </td>
+            </tr>
+            <?php endforeach; endif; else: echo "" ;endif; ?>
+            </tbody>
+        </table>
+    </div>
+    <div id="pages"></div>
+</div>
 </div>
 <script>
 </script>
@@ -138,10 +137,18 @@
         });
 
 
-        // 添加 分类
-        // $('.add').on('click', function () {
-        //     layObj.dialog("<?php echo url('add'); ?>")
-        // });
+        // 添加 规格
+        $('.add').on('click', function () {
+            layObj.dialog("<?php echo url('admin/category/add'); ?>")
+        });
+
+        // 编辑 规格
+        $('.update').on('click', function () {
+            let id = $(this).attr('data-id'); // fu
+            let url = '<?php echo url("update"); ?>?id=' + id
+            layObj.dialog(url)
+        })
+
 
         //监听状态 更改
         form.on('switch(switchStatus)', function (obj) {
